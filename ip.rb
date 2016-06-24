0717234f3a9e01f0ad5be1a615366b0ac84803c5
Zach Vickery <zvickery@gmail.com>
2016-06-23 23:27:59 -0600

    Fix to work with nc.

--- ip.rb
+++ ip.rb
@@ -9,13 +9,19 @@ logfile = "#{File.expand_path(File.dirname(__FILE__))}/ip.log"
 logger = Logger.new(logfile)
 logger.level = Logger::INFO
 
-server = TCPServer.new port
-loop do
-  Thread.start(server.accept) do |client|
-    remote = client.addr[2]
-    client.recvfrom(512)
-    client.puts "#{remote}\n"
-    logger.info("Client: #{remote}")
-    client.close
+server = TCPServer.new(port)
+begin
+  loop do
+    Thread.start(server.accept) do |client|
+      remote = client.peeraddr[2]
+      if IO.select([client], [], [], timeout=0)
+        client.recv(512)
+      end
+      client.puts "#{remote}\n"
+      logger.info("Client: #{remote}")
+      client.close
+    end
   end
+rescue Interrupt
+  puts
 end
